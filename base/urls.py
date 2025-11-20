ba34c0d6fa76a23335989e4abbd75d7abda8ef0c
Alexander Pavlinov <telomerazaferment@gmail.com>
2025-11-20 09:33:40 +0300

    Заполнили файл urls для cart и дописали в urls base путь для cart

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static   # 27.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls', namespace='shop')),  # 19.
+    path('cart/', include('cart.urls', namespace='cart')),  # 39.
 ]
 
 # 25.
