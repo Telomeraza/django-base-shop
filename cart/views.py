0334bf40047d1b12e563d8bc9b6016dae06cce94
Alexander Pavlinov <telomerazaferment@gmail.com>
2025-11-20 09:06:24 +0300

    Создали файл forms и описали его

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_list_or_404    # 31.
+from shop.models import Product                                    # 32.
+from .cart import Cart                                              # 33.
 
 # Create your views here.
